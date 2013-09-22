b3f5a12b0eca6e391cc43623371c124e175256bb
knazarov <mail@kn.am>
2013-09-22 13:29:32 +0400

    specify proper imports in __init__.py

--- jira2json/__init__.py
+++ jira2json/__init__.py
@@ -1,4 +1,8 @@
-from jira2json import dump
+from jira2json import establish_http_session
+from jira2json import get_credentials
+from jira2json import search_issues
+from jira2json import get_issue
+
 
 # Follows semantic versioning http://semver.org/
 __version__ = '0.0.1-dev'
